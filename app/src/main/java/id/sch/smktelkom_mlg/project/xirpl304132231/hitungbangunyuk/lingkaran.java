f323adde3f747da24b2ace60cad5baffe256ff20
Sandro <9c.sandro@gmail.com>
2016-11-14 20:15:54 +0700

    digawe ngene a?

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl304132231/hitungbangunyuk/lingkaran.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl304132231/hitungbangunyuk/lingkaran.java
@@ -1,21 +1,20 @@
 package id.sch.smktelkom_mlg.project.xirpl304132231.hitungbangunyuk;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class lingkaran extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lingkaran);
 
-        findViewById(R.id.lingkaran).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.buttonbacklingkaran).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,lingkaran.class));
+                onBackPressed();
             }
         });
     }
