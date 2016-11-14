1143525daa50192d48f8e536c8d882ba268191df
Sandro <9c.sandro@gmail.com>
2016-11-14 20:48:57 +0700

    digawe ngene a? (2)

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl304132231/hitungbangunyuk/lingkaran.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl304132231/hitungbangunyuk/lingkaran.java
@@ -3,13 +3,27 @@ package id.sch.smktelkom_mlg.project.xirpl304132231.hitungbangunyuk;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class lingkaran extends AppCompatActivity {
 
+    EditText etJari;
+    TextView tvHasil;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lingkaran);
+
+        etJari = (EditText) findViewById(R.id.editTextJari);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+
+        findViewById(R.id.buttonOk).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doHitung();
+            }
+        });
 
         findViewById(R.id.buttonbacklingkaran).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -17,5 +31,11 @@ public class lingkaran extends AppCompatActivity {
                 onBackPressed();
             }
         });
+    }
+
+    private void doHitung() {
+        int a = etJari.getText().toString().isEmpty()?0:Integer.parseInt(etJari.getText().toString());
+
+        tvHasil.setText((int) (3.14*a*a));
     }
 }
